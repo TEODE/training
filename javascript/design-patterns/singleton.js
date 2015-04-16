053d8ac9578048e2c9a4786b176e3b3ed6514850
Vincent Giraud <vincent.giraud@teode.com>
2015-04-16 23:09:14 +0800

    Singleton pattern _static emulation

--- javascript/design-patterns/singleton.js
+++ javascript/design-patterns/singleton.js
@@ -50,3 +50,47 @@ var Singleton = (function () {
 
 // calling public methods is then as easy as
 Singleton.getInstance().publicMethod();
+
+// One last example
+var SingletonTester = (function () {
+   // options: an object containing configuration options for the singleton
+   // e.g. var options = {name: 'test', pointC: 5};
+    function Singleton(options) {
+
+        // set option to the options supplied or an empty object if none provided
+        options = options || {};
+        // set the name parameter
+        this.name = 'SingletonTester';
+        var args = Array.prototype.slice.call(arguments)[0];
+        console.log(arguments);
+        // set the value of pointX
+        this.pointX = args.pointX || 6;
+        // set the value of pointY
+        this.pointY = args.pointY || 10;
+    }
+    // this is our instance holder
+    var instance;
+
+    // this is an emulation of static variables and methods
+    var _static = {
+
+        name: 'SingletonTester',
+
+        // This is a method for getting an instance
+        // It returns a singleton instance of a singleton object
+        getInstance: function (options) {
+            if (instance === undefined) {
+                instance = new Singleton(options);
+            }
+            return instance;
+        }
+    };
+    return _static;
+})();
+
+var singletonTest = SingletonTester.getInstance({
+    pointX: 5,
+    pointY: 12
+});
+console.log(singletonTest.pointX); // outputs 5
+console.log(singletonTest.pointY); // outputs 12
