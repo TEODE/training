a182e3626e8b40ea207c113323cdec9ca6a65e53
Vincent Giraud <vincent.giraud@teode.com>
2015-04-16 22:37:30 +0800

    Singleton pattern

--- javascript/design-patterns/singleton.js
+++ javascript/design-patterns/singleton.js
@@ -23,3 +23,30 @@ var mySingleton = function () {
 var single = mySingleton();
 single.publicMethod(); // logs 'something private'
 console.log(single.publicVar); // logs 'the public can see this!'
+
+// To save on resources, we can place the instanciation code inside another constructore function
+var Singleton = (function () {
+    var instantiated;
+
+    function init() {
+        // singleton here
+        return {
+            publicMethod: function () {
+                console.log("hello world");
+            },
+            publicProperty: 'test'
+        }
+    }
+
+    return {
+        getInstance: function () {
+            if (!instantiated) {
+                instantiated = init();
+            }
+            return instantiated;
+        }
+    }
+})();
+
+// calling public methods is then as easy as
+Singleton.getInstance().publicMethod();
