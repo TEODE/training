525bb9f3b0068e11e54935787a7bd13256bbdc08
Vincent Giraud <vincent.giraud@teode.com>
2015-04-23 00:12:57 +0800

    Observer pattern second part

--- javascript/design-patterns/observer.js
+++ javascript/design-patterns/observer.js
@@ -25,4 +25,41 @@ var pubsub = {};
 
         return this;
     };
-});
+
+    // Subscribe to events of interest
+    // with a specific topic name and a
+    // callback function, to be executed
+    // when the topic/event is observed
+    q.subscribe = function ( topic, func ) {
+
+        if (!topics[topic]) {
+            topics[topic] = [];
+        }
+
+        var token = (++subUid).toString();
+        topics[topic].push({
+            token: token,
+            func: func
+        });
+        return token;
+
+    };
+
+    // Unsubscribe from a specific
+    // topic, based on a tokenized reference
+    // to the subscription
+    q.unsubscribe = function( token ) {
+        for ( var m in topics ) {
+            if ( topics[m] ) {
+                for (var i = 0, j = topics[m].length; i < j; i++) {
+                    if (topics[m][i].token === token) {
+                        topics[m].splice(i, 1);
+                        return token;
+                    }
+                }
+            }
+        }
+    };
+})( pubsub );
+
+
