0fc1da6b870853101c8b2594a61f410c4a9123dd
Vincent Giraud <vincent.giraud@teode.com>
2015-05-18 13:13:27 +0800

    Differential inheritance with prototype pattern

--- javascript/design-patterns/prototype.js
+++ javascript/design-patterns/prototype.js
@@ -11,4 +11,23 @@ var someCar = {
 var anotherCar = Object.create( someCar );
 console.log(anotherCar.name); // Now you'll hopefully see that one is a prototype of the other
 
+// advanced concepts such as differential inheritance
+var vehicule = {
+    getModel: function () {
+        console.log('The model of the is vehicule is..' + this.Model);
+    }
+};
 
+var car = Object.create(vehicule, {
+    'id': {
+        value: 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+            return v.toString(16);
+        }),
+        enumerable: true // writable: false, configurable: false by default
+    },
+    'model': {
+        value: 'Ford',
+        enumerable: true
+    }
+});
