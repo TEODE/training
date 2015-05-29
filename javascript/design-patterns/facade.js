f159318bf3b6af1e8fd9e068213349374887ebd6
Vincent Giraud <vincent.giraud@teode.com>
2015-05-29 23:31:57 +0800

    Facade and module patterns

--- javascript/design-patterns/facade.js
+++ javascript/design-patterns/facade.js
@@ -14,3 +14,33 @@ var addMyEvent = function( el,ev,fn ){
         el['on' + ev] = fn;
     }
 };
+
+// Facade + Module patterns. Facade simplify the access to the methods
+
+var module = (function() {
+    var _private = {
+        i:5,
+        get: function() {
+            console.log('current value' + this.i);
+        },
+        set: function( val ) {
+            this.i = val;
+        },
+        run: function () {
+            console.log('running');
+        },
+        jump: function () {
+            console.log('jumping');
+        }
+    };
+    return {
+        facade: function( args ) {
+            _private.set(args.val);
+            _private.get();
+            if ( args.run ) {
+                _private.run();
+            }
+        }
+    }
+}());
+module.facade({run: true, val:10}); //outputs current value: 10, running
