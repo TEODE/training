04389023184640c670a90a6c681c7f1ab726c2e5
Vincent Giraud <vincent.giraud@teode.com>
2015-07-03 00:11:50 +0800

    Add augment method for mixin

--- javascript/design-patterns/mixin.js
+++ javascript/design-patterns/mixin.js
@@ -18,3 +18,35 @@ Mixin.prototype = {
         console.log('drive backward');
     }
 };
+
+// Augment existing 'class' with a method from another one
+function augment ( receivingClass, givingClass) {
+    // only proide certain methods
+    if ( arguments[2] ) {
+        for (var i = 2, len=arguments.length; i<len; i++) {
+            receivingClass.prototype[arguments[i]] = givingClass.prototype[arguments[i]];
+        }
+    }
+    // provide all methods
+    else {
+        for ( var methodname in givingClass.prototype ) {
+            /* check to make sure the receiving class doesn't
+            have a method of the same name as the one currently
+            being processed
+             */
+            if (!receivingClass.prototype[methodName]) {
+                receivingClass.prototype[methodname] = givingClass.prototype[methodname];
+            }
+        }
+    }
+};
+
+// Augment the Car with the methods 'driveForward' and 'driveBackward'
+augment( Car, Mixin, 'driveForward', 'driveBackward' );
+
+// Create a new Car
+var vehicle = new Car({model:'Ford Escort', colour: 'blue'});
+
+// Test to make sure we now have access to the methods
+vehicle.driveForward();
+vehicle.driveBackward();
