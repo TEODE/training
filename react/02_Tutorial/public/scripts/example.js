bf45dab3099fe4d4ec7c8099968df89e6d6bb847
Vincent Giraud <vincent.giraud@teode.com>
2015-10-05 19:21:54 +0800

    Composing Components

--- react/02_Tutorial/public/scripts/example.js
+++ react/02_Tutorial/public/scripts/example.js
@@ -1,15 +1,36 @@
 var CommentBox = React.createClass({
-  render: function() {
-    return (
-        React.createElement('div', {classame: "commentBox"},
-            "Hello, world! I'm a CommentBox."
-        )
-    );
-  }
+    render: function() {
+        return (
+            <div className="commentBox">
+                <h1>Comments</h1>
+                <CommentList />
+                <CommentForm />
+            </div>
+        );
+    }
+});
+
+var CommentList = React.createClass({displayName: "CommentList",
+    render: function() {
+        return (
+            React.createElement('div', {className: "commentList"},
+                "Hello I am a CommmentList."
+            )
+        );
+    }
+});
+
+var CommentForm = React.createClass({displayName: "CommentForm",
+    render: function() {
+        return (
+            React.createElement('div', {className: "commentForm"},
+                "Hello I am a CommentForm."
+            )
+        );
+    }
 });
 
 React.render(
-    React.createElement('CommentBox', null),
+    <CommentBox />,
     document.getElementById('content')
 );
-
