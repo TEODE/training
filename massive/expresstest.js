9b513fc8e009a224ba0c15b72a5b4334e4738081
Vincent Giraud <vincent.giraud@teode.com>
2016-01-07 18:37:00 +0800

    MassiveJS Full JSONB Document Support example

--- massive/expresstest.js
+++ massive/expresstest.js
@@ -42,3 +42,53 @@ db.Users.search({columns: ["Email"], term: "test@test.com"}, function(err, users
     // all users with the word 'test' in their email
     //console.log(users);
 });
+
+// Full JSONB Document Support
+var newDoc = {
+    title: "Chicken Ate Nine",
+    description: "A book about chickens of Kauai",
+    price: 99.00,
+    tags: [
+        {name: "Simplicity", slug: "simple"},
+        {name: "Fun for all", slug: "fun-for-all"}
+    ]
+};
+
+db.saveDoc("my_documents", newDoc, function(err, res) {
+    //the table my_documents was created on the fly
+    //res is the new document with an ID created for you
+    //console.log(res);
+});
+
+//full text search...
+db.my_documents.searchDoc({
+    keys : ["title", "description"],
+    term : "Kauai"
+}, function(err,docs){
+    //docs returned with an on-the-fly Full Text Search for 'Kauai'
+    //console.log(docs);
+});
+
+//comparative queries - these don't use indexing
+db.my_documents.findDoc({"price >": 50.00}, function(err,docs){
+    //1 or more documents returned with a price > 50
+    //console.log(docs);
+});
+
+//run a deep match passing an array of objects
+//again flexing the index we created for you
+db.my_documents.findDoc({tags: [{slug : "simple"}]}, function(err,docs){
+    //1 or more documents returned
+    //console.log(docs);
+});
+
+//NOT IN
+db.my_documents.findDoc({"id <>": [3,5]}, function(err,docs){
+    //documents without ID 3 and 5
+    //console.log(docs);
+});
+
+//delete all records!
+db.my_documents.destroy({}, function(err, res) {
+    // FIXIT: JSON-style matcher doesn't work
+});
